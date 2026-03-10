7b05f6433b6dfd14701f46163bb18c37f0947b3f
Dmitry <dim4ik121313@gmail.com>
2026-03-10 12:25:45 +0400

    push prototype

--- TextAnalyzer.Api/Program.cs
+++ TextAnalyzer.Api/Program.cs
@@ -4,24 +4,18 @@ using TextAnalyzer.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args); 
 builder.Services.AddControllers(); 
-builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddEndpointsApiExplorer();
 
-// 1. Создаем билдер для источника данных Npgsql
 var dataSourceBuilder = new NpgsqlDataSourceBuilder(builder.Configuration.GetConnectionString("DefaultConnection"));
 
-// 2. ВКЛЮЧАЕМ поддержку JSON (то самое "explicit opt-in")
 dataSourceBuilder.EnableDynamicJson();
 
-// 3. Строим источник данных
 var dataSource = dataSourceBuilder.Build();
 
-// 4. Регистрируем DbContext, используя этот dataSource
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(dataSource));
 
 
-// builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-// builder.Services.AddSwaggerGen(); 
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy => { policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); });
@@ -29,8 +23,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// if (app.Environment.IsDevelopment())
-// { app.UseSwagger(); app.UseSwaggerUI(); }
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -39,7 +31,6 @@ if (!app.Environment.IsDevelopment())
 else
 {
     app.UseDeveloperExceptionPage();
-    // app.UseMigrationsEndPoint();
 }
 
 app.UseCors();
@@ -50,7 +41,6 @@ app.Use(async (context, next) =>
     if (context.Request.QueryString.HasValue)
         Console.WriteLine($"Query: {context.Request.QueryString}");
 
-    // Если есть тело — читаем
     if (context.Request.ContentLength > 0)
     {
         context.Request.EnableBuffering();
